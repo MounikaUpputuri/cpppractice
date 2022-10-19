b9be75919f41f48e0d025400df3dc784708429ed
Mounika Upputuri <mouni.upputuri@gmail.com>
2022-10-19 21:57:53 +0200

    Hi

--- TestingOfEquality.cpp
+++ TestingOfEquality.cpp
@@ -4,12 +4,53 @@ int main()
 {
     bool result_equal{false};
     bool result_notequal{false};
-    int num1{},num2{};
+    /*int num1{},num2{};
     std::cout<<boolalpha;
     cout<<"enter the 2 numbers"<<endl;
     cin>>num1>>num2;
     result_equal=(num1==num2);
     result_notequal=(num1!=num2);
     cout<<"comparision result for equal is:"<<result_equal<<endl;
-    cout<<"comparision result for equal is:"<<result_notequal<<endl;
+    cout<<"comparision result for equal is:"<<result_notequal<<endl;*/
+
+    /*char char1{},char2{};
+    std::cout<<boolalpha;
+    cout<<"enter two characters"<<endl;
+    cin>>char1>>char2;
+    result_equal=(char1==char2);
+    result_notequal=(char1!=char2);
+    cout<<"comparision result for equal is :"<<result_equal<<endl;
+    cout<<"comparision result for equal is :"<<result_notequal<<endl;*/
+
+    /*double double1{},double2{};
+    std::cout<<boolalpha;
+    cout<<"enter two double values"<<endl;
+    cin>>double1>>double2;
+    result_equal=(double1==double2);
+    result_notequal=(double1!=double2);
+    cout<<"comparision result for equal is :"<<result_equal<<endl;
+    cout<<"comparision result for equal is :"<<result_notequal<<endl;*/
+
+    double double1{};
+    int num1{};
+    std::cout<<boolalpha;
+    cout<<"enter the integre and double values"<<endl;
+    cin>>num1>>double1;
+    result_equal=(num1==double1);
+    result_notequal=(num1!=double1);
+    cout<<"comparision result for equal is :"<<result_equal<<endl;
+    cout<<"comparision result for equal is :"<<result_notequal<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
