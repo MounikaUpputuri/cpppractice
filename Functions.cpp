71defd521ce43aa33f2dde06579dabda01ab609a
Mounika Upputuri <mouni.upputuri@gmail.com>
2022-10-31 19:53:11 +0100

    Hi

---
+++ Functions.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+//math example
+int main()
+{
+    int num{};
+    cout<<"enter the number"<<endl;
+    cin>>num;
+    cout<<"the square root of the num is: "<<sqrt(num)<<endl;
+    cout<<"cube root of the number is: "<<cbrt(num)<<endl;
+    cout<<"ciel of the number is:" <<ceil(num)<<endl;
+    cout<<"ciel of the number is:" <<sin(num)<<endl;
+    cout<<"ciel of the number is:" <<cos(num)<<endl;
+    cout<<"ciel of the number is:" <<floor(num)<<endl;
+    cout<<"ciel of the number is:" <<round(num)<<endl;
+    double power{};
+    cout<<"enter the power"<<endl;
+    cin>>power;
+    cout<<num<<" is raised to the power"<<pow(num, power)<<endl;
+
+
+
+
+
+
+
+
+
+
+
+
+}
